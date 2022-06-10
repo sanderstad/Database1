cea8503ecdc61f7f7ecf88067c1597fbd424b54d
Sander Stad <sanderstad@gmail.com>
2022-06-10 11:22:44 +0200

    changed total from quantity to unitprice

--- Database1/Stored Procedures/CustOrderHist.sql
+++ Database1/Stored Procedures/CustOrderHist.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE CustOrderHist @CustomerID nchar(5)
 AS
-SELECT ProductName, Total=SUM(Quantity)
+SELECT ProductName, Total=SUM(UnitPrice)
 FROM Products P, [Order Details] OD, Orders O, Customers C
 WHERE C.CustomerID = @CustomerID
 AND C.CustomerID = O.CustomerID AND O.OrderID = OD.OrderID AND OD.ProductID = P.ProductID
